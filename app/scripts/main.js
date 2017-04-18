3ea269e395c50c030a87826d4f444512df7e1715
Jon Manock <jonmanock@gmail.com>
2017-04-18 07:45:21 -0400

    js compiles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,80 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  $('.movies').hide();
+  $('.mDots').hide();
+
+  $('.pictures').on('click', function(){
+    $('.movies').hide();
+    $('.mDots').hode();
+    $('.slideshow-container').show();
+    $('.pDots').show();
+  });// End `.pictures`
+
+  $('.videoLink').on('click', function(){
+    $('.movies').show();
+    $('.mDots').show();
+    $('.slideshow-container').hide();
+    $('.pDots').hide();
+    showSlides(slideIndex, 'v');
+  });// End `.videoLink`
+
+  $('.dot').on('click', function(){
+    var n = $(this).attr('target');
+    currentSlide(n, 'p');
+  });// End `.dot`
+
+  $('.vDot').on('click', function(){
+    var n = $(this).attr('target');
+    currentSlide(n, 'v');
+  });// End `.vDot`
+
+  $('.mNext').on('click', function(){
+    plusSlides(1, 'v');
+  });// End `.mNext`
+
+  $('.mPrev').on('click', function(){
+    plusSlides(-1, 'v');
+  });// End `.mPrev`
+
+  $('.next').on('click', function(){
+    plusSlides(1, 'p');
+  });//End `.next`
+
+  $('.prev').on('click', function(){
+    plusSlides(-1, 'p');
+  });// End `.prev`
+
+  var slideIndex = 1;
+  showSlides(slideIndex, 'p');
+
+  function plusSlides(n,x){
+    showSlides(slideIndex += n, x);
+  }
+
+  function currentSlide(n,x){
+    showSlides(slideIndex = n, x);
+  }
+
+  function showSlides(n,x){
+    if(x === 'v'){
+      var slides = $('.myVslides');
+      var dots = $('.vDot');
+    }else{
+      var slides = $('.mySlides');
+      var dots = $('.dot');
+    }
+    if(n > slides.length){
+      slideIndex = 1;
+    }
+    if(n < 1){
+      slideIndex = slides.length;
+    }
+    for(var i = 0; i<slides.length; i++){
+      slides[i].style.display = 'none';
+    }
+    for(var i = 0; i<dots.length; i++){
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[slideIndex -1].style.display = 'block';
+    dots[slideIndex -1].className += ' active';
+  }
+});// End `Document`
