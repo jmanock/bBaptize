1e1d3b683db9aff922e3bd30bb72020ac09fd259
Jon Manock <jonmanock@gmail.com>
2017-04-18 03:02:49 -0400

    fixing bugs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,75 @@
-console.log('\'Allo \'Allo!');
+ $(document).ready(function(){
+//   $('.video').hide();
+//   $('.mDots').hide();
+
+  $('.pictures').on('click', function(){
+
+  });// End `.Pictures`
+
+  $('.movies').on('click', function(){
+    console.log('this is movies');
+  });// End `.Movies`
+
+  $('.dot').on('click', function(){
+    var n = $(this).attr('target');
+    currentSlide(n,'p');
+  });// End `.Dot`
+
+  $('.vDot').on('click', function(){
+    var n = $(this).attr('target');
+    currentSlide(n, 'p');
+  });// End `.vDot`
+
+  $('.mNext').on('click', function(){
+    plusSlides(1, 'v');
+  });// End `.mNext`
+
+  $('.mPrev').on('click', function(){
+    plusSlides(-1, 'v');
+  });// End `.mPrev`
+
+  $('.next').on('click', function(){
+    plusSlides(1, 'p');
+  });// End `.Next`
+
+  $('.prev').on('click', function(){
+    plusSlides(-1, 'p');
+  });// End `.Prev`
+
+ });// End `Ready`
+
+var slideIndex = 1;
+showSlides(slideIndex, 'p');
+
+function plusSlides(n,x){
+  showSlides(slideIndex += n, x);
+}
+
+function currentSlide(n,x){
+  showSlides(slideIndex = n, x);
+}
+
+function showSlides(n,x){
+  if(x === 'v'){
+    var slides = $('.myVslides');
+    var dots = $('.vDot');
+  }else{
+    var slides = $('.mySlides');
+    var dots = $('.dot');
+  }
+  var i;
+  if(n > slides.length){
+    slideIndex = 1;
+  }
+  if(n < 1){
+    slideIndex = slides.length;
+  }
+  for(i = 0; i < slides.length; i++){
+    slides[i].style.display = 'none';
+  }
+  for(i = 0; i < dots.length; i++){
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex -1].style.display = 'block';
+  dots[slideIndex -1].className += ' active';
+}
